2da774dc69790af8737c7f8a3006553331b1533c
Ana Carbonell Prieto <anacarpri@alu.edu.gva.es>
2023-12-14 15:45:59 +0100

    Initial commit

---
+++ src/main/java/ana/andrea/dragonball/Proyecto_DragonBall_Ana_Andrea.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ana.andrea.dragonball;
+
+/**
+ *
+ * @author enana14
+ */
+public class Proyecto_DragonBall_Ana_Andrea {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
