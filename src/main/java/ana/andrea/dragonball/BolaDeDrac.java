63aefa4eff250ebe9226eecfa7d585a0d7a23ef6
Ana Carbonell Prieto <anacarpri@alu.edu.gva.es>
2023-12-16 11:16:15 +0100

    Lógica nivel 5, java con mensajes, método para respuestas y corregido nombres

--- src/main/java/ana/andrea/dragonball/BolaDeDrac.java
+++ src/main/java/ana/andrea/dragonball/BolaDeDrac.java
@@ -13,62 +13,34 @@ import java.util.Scanner;
  */
 public class BolaDeDrac {
 
-    public static Scanner input;
-    final static String SIGNO_SUMA = "+";
-    final static String ASTERISCO = "*";
-    final static int numMinFactorial = 1;
-    final static String VOCALES = "aeiou";
-    public static int B1, B2, B3, B4, B5, B6, B7;
+    private static Scanner input;
+    private static int b1, b2, b3, b4, b5, b6, b7;
+    private final static String SUMA = "+";
+    private final static String MULT = "*";
+    private final static int NUM_MIN_FACTORIAL = 1;
+    private final static String VOCALES = "aeiou";
     
     public static void main(String[] args) {
         input = new Scanner(System.in);
-        inicio();
-        nivel2();
-        nivel3();
-        
     }
     
-    public static void inicio(){
-        String mensaje = "BENVINGUTS A L’AVENTURA DE DRAGON BALL\n"
-                + "========================================\n"
-                + "Son-Goku es troba tranquil·lament a casa junt al seu fill Son-Gohan, a\n"
-                + "qui li està ajudant a resoldre uns deures que li han manat fer a l’escola,\n"
-                + "concretament son de matemàtiques. De sobte, apareix a casa el Fullet\n"
-                + "Tortuga i els conta que el malvat villà Freezer està planejant trobar les\n"
-                + "7 boles del drac per tal de demanar un desig i obtindre un poder que\n"
-                + "ningú podrà mai parar. El planeta està en perill. El Fullet Tortuga et\n"
-                + "demana ajuda ¿Vols que Son-Goku i Son-Gohan accepten el repte?";
-        if(validacionErrores()== 1){
-            nivel1(0,30);
-        }
-        fin();
+    private static boolean mostrarInicio() {
+        String mensaje = Mensaje.INICIO;
+        System.out.println(mensaje);
+        return validarRespuesta(pedirEntero("Introdueix un 1 vols que l'accepten: "), 1);
     }
     
-    public static int nivel1(int n1, int n2){
-        String mensaje = String.format(
-                "Son Goku: Molt bé! Anem a buscar les boles de drac abans que\n"
-                + "Freezer puga tenir-les. --va dir Son-Goku des de la seua casa en el\n"
-                + "número n1 de Ciutat del Nord.\n"
-                + "Goku va recordar que l’última vegada que les boles van ser repartides\n"
-                + "pel món de manera aleatòria, una d’elles va anar a parar a la zona on\n"
-                + "actualment viu el seu amic Satanàs Cor Petit i que probablement ell la\n"
-                + "tinga en el seu poder. Van marxar doncs cap al número n2 de la Ciutat\n"
-                + "Meravella, que és on ell viu.\n"
-                + "Després d’un llarg camí, hem arribat a la casa de Satanàs i, en efecte,\n"
-                + "ell té una de les boles. Però no vol donar-nos-la gratuïtament, ja que li\n"
-                + "té molta estima. Ens proposa la següent lluita matemática:\n"
-                + "Satanàs Cor Petit: Sabríeu dir-me quin és el sumatori entre el número\n"
-                + "de la vostra casa i el número de la meua? Si l’endevineu, vos donaré\n"
-                + "la bola i m’uniré al vostre equip."
-        );
-        n1 = generarNumAleatorio(10, 0);
-        n2 = generarNumAleatorio(30, 20);
+    private static int mostrarNivel1() {
+        int n1 = generarNumAleatorio(10, 0);
+        int n2 = generarNumAleatorio(30, 20);
+        String mensaje = String.format(Mensaje.LVL1, n1, n2);
+        System.out.println(mensaje);
         //n1 = 10; ESTO ERA PROBAR QUE SÍ FUNCIONABA, LO DEJO POR SI
         //n2 = 20;
         int resultado = 0;
 
         for (int x = n1; x <= n2; x++) {
-            System.out.print(x + ((x==n2)?"":SIGNO_SUMA));
+            System.out.print(x + ((x==n2)?"":SUMA));
             resultado += x;
         }
 
@@ -78,29 +50,14 @@ public class BolaDeDrac {
     }
 
     
-    public static String nivel2(){
-        String mensaje = String.format(
-                "Satanàs Cor Petit: Sou uns cracks de les matemàtiques! Pensava que\n"
-                + "només sabíeu lluitar. Ací teniu la bola de b1 estreles. Per cert, tinc el\n"
-                + "número de telèfon de Ten Shin Han, qui em va enviar un WhatsApp la\n"
-                + "setmana passada per contar-me que havia trobat la bola de b2 estreles\n"
-                + "en el Desert de l’Oblit. Anem-hi!\n"
-                + "Només arribar, trobaren a Ten Shin Han el qual no tenia el seu millor\n"
-                + "dia i no els van rebre amb bona cara. Estava molt cabrejat perquè\n"
-                + "estava intentant resoldre un enigma matemàtic i no veia forma de ferho.\n"
-                + "Son Gohan: Hola Ten Shin Han, necessitem que ens dones la teua\n"
-                + "bola de drac.\n"
-                + "Ten Shin Han: Sí, un moment, resulta que porte així com 3 hores per\n"
-                + "a descrifrar un enigma alfabètic anomenat “La mescla sagrada de\n"
-                + "cadenes de text” i no veig la solució. Si m’ajudeu a resoldre’l, vos\n"
-                + "donaré la bola. "
-        );
+    private static String mostrarNivel2() {
+        String mensaje = String.format(Mensaje.LVL2, b1, b2);
         //StringBuilder cadena = new StringBuilder();
         //String cadena1 = "erjw3";
         //String cadena2 = "A3q2q";
         
-        String cadena = generarCadena(B2);
-        String cadena2 = generarCadena(B2);
+        String cadena = generarCadenaVocales(b2);
+        String cadena2 = generarCadenaVocales(b2);
         
         String cadenaResultado = "";
         
@@ -115,60 +72,27 @@ public class BolaDeDrac {
         return cadenaResultado;
     }
 
-    public static int nivel3() {
-        String mensaje = String.format(
-                "Ten Shin Han: Wow! Gràcies, ací teniu la bola de b2 estreles. Per cert,\n"
-                + "vaig saber que el nostre enemic Boo va trobar una bola de drac en la\n"
-                + "seua mudança a “Muntanya Perduda”. Vos guiaré a la cova de la\n"
-                + "muntanya on viu. No és lluny d’ací.\n"
-                + "Boo es trobava descansant a la cova. Ja que va tindre molts problemes\n"
-                + "en els seus anteriors enfrontaments amb Goku aquest no tenia intenció\n"
-                + "de lluitar més amb ell, però al veure’l què anava a per la seua estimada\n"
-                + "bola de drac de b3 estelles, no va reaccionar amb bones maneres.\n"
-                + "Boo: Què voleu? La meua bola de drac?\n"
-                + "Son Goku: Necessitem reunir-les totes abans que Freezer. El món\n"
-                + "corre un gran perill! Sabem que tú ja no vols fer mal al planeta. T’ho\n"
-                + "demanem per favor, pel bé de tots.\n"
-                + "Boo: Però és meua! La vaig trobar jo! Encara que pensant-ho millor,\n"
-                + "només vos la donaré si em dieu la resposta correcta a la solució de\n"
-                + "quin és el factorial de b3."
-        );
+    private static int mostrarNivel3() {
+        String mensaje = String.format(Mensaje.LVL3, b2, b3, b3);
         //B3=5; ESTO ERA PARA PROBAR
 
-        int resultado = numMinFactorial;
-        for(int i = B3; i >= numMinFactorial; i--){
-            System.out.print(i + ( (i == numMinFactorial)?"":ASTERISCO));
+        int resultado = NUM_MIN_FACTORIAL;
+        for(int i = b3; i >= NUM_MIN_FACTORIAL; i--){
+            System.out.print(i + ( (i == NUM_MIN_FACTORIAL)?"":MULT));
             resultado *=i;
         }
         System.out.println("\nEl resultado es: " + resultado);
         return resultado;
     }
     
-    public static boolean nivel4() throws InterruptedException {
+    private static boolean mostrarNivel4() throws InterruptedException {
         int n = generarNumAleatorio(8, 12);
-        String mensaje = String.format("""
-                                       Boo: Vaja, vos ho he posat realment fácil. Ací teniu la bola. Ara bé, us\n"
-                                       avance que encara teniu un os dur de rosegar. Cèl·lula també les\n"
-                                       buscava i vaig escoltar en la tele que ja tenia unes quantes recollides.\n"
-                                       "Si no recorde mal, el vaig sentir a les notícies del canal número %d de\n"
-                                       "la meua televisió.\n"
-                                       "Van sintonitzar el canal %d i van confirmar que Cèl·lula es trobava en\n"
-                                       "“Ciutat Nova”. Ràpidament van anar a enfrontar-se amb ell i així\n"
-                                       "aconseguir les boles que tenia en el seu poder.\n"
-                                       "Son Gohan: Hola Cèl·lula, no volem problemes amb tu. Per favor,\n"
-                                       "dona'ns les teues boles de drac i no et farem mal.\n"
-                                       "Cèl·lula: Jajaja. He entrenat des de l'última lluita i ara no podreu amb\n"
-                                       "mi i, per descomptat, no podreu llevar-me les boles del drac que tinc\n"
-                                       "guardades a aquesta caixa. Juguem a un joc, la caixa té una\n"
-                                       "contrasenya, però l’única manera d’aconseguir-la és desxifrant\n"
-                                       "aquesta endevinalla, juguem?
-                                       """, n, n
-        );
+        String mensaje = String.format(Mensaje.LVL4, n, n);
         System.out.println(mensaje);
         
-        String cadena1 = generarCadena(n);
-        String cadena2 = generarCadena(n);
-        String cadena3 = generarCadena(n);
+        String cadena1 = generarCadenaVocales(n);
+        String cadena2 = generarCadenaVocales(n);
+        String cadena3 = generarCadenaVocales(n);
         
         String cadenas = String.format("""
                           Cadena 1: %s
@@ -178,39 +102,37 @@ public class BolaDeDrac {
         
         System.out.println(cadenas);
         
-        String cadena = generarCadena(2);
+        String cadena = generarCadenaVocales(2);
         System.out.println(cadena);
         
         return true;
     }
     
-    public static boolean nivel5() {
-        String mensaje = String.format(
-                "Cèl·lula: Maleïts sigueu! L’heu endevinat, però.... he de confessar-vos\n"
-                + "que anava de fanal, i que no tinc cap bola de drac. El que sí puc dir-\n"
-                + "vos es que la resta de boles que neccesiteu las té Freezer. Aneu a\n"
-                + "molestar-lo a ell. Adèu!\n"
-                + "Son Goku: Bé, anem corrent al planeta de Freezer a llevar-li les boles\n"
-                + "que resten. Esperem que surta tot bé.\n"
-                + "Van volar al Planeta Infernal on Freezer esperava al seus contrincants\n"
-                + "per a la lluita final.\n"
-                + "Son Goku: Hola Freezer, sabem què trames. No ho permetrem.\n"
-                + "Dona'ns les boles de drac b4, b5, b6 i b7 que sabem que tens.\n"
-                + "Freezer: De cap manera! Doneu-me vosaltres les vostres. Però mireu,\n"
-                + "com no vull lluitar perquè ja tinc una edat, us done la possibilitat de\n"
-                + "resoldre aquest conflicte amb una lluita matemàtica. Si sabeu dir-me\n"
-                + "quin és el mínim comú múltiple de b4, b5, b6 i b7 tot haurà acabat."
-        );
-        return true;
+    private static boolean mostrarNivel5() {
+        String mensaje = String.format(Mensaje.LVL5, b4, b5, b6, b7, b4, b5, b6, b7);
+        int respuesta = calcularMCM(b4, b5);
+        respuesta = calcularMCM(respuesta, b6);
+        respuesta = calcularMCM(respuesta, b7);
+        return validarRespuesta(pedirEntero(mensaje), respuesta);
     }
-    public static void fin(){
-        System.out.println("THE END");
-    }
-            
-            
-    public static int validacionErrores() {
+    
+    private static int calcularMCM(int a, int b) {
+        int num1 = a;
+        int num2 = b;
+        
         do {
-            //System.out.print(respuesta);
+            int resto = num1 % num2;
+            num1 = num2;
+            num2 = resto;
+            if (resto == 0) break;
+        } while (true);
+        
+        return a * b / num1;
+    }      
+            
+    private static int pedirEntero(String mensaje) {
+        do {
+            System.out.print(mensaje);
             if (input.hasNextInt()){
                 return input.nextInt();
             }
@@ -219,34 +141,47 @@ public class BolaDeDrac {
         } while (true);
     }
     
-        public static String validacionErrores(String respuesta) {
+    private static String pedirCadena(String mensaje) {
         do {
-            System.out.print(respuesta);
-            if (input.hasNextLine()){
-                return input.nextLine();
+            System.out.print(mensaje);
+            if (input.hasNext()){
+                return input.next();
             }
             System.out.println("Error! El tipo de datos introduidos es incorrecto");
-            input.nextLine();
+            input.next();
         } while (true);
     }
     
-    
-    public static void generarBolas() {
-        B1 = generarNumAleatorio(3, 1);
-        B2 = generarNumAleatorio(7, 4);
-        B3 = generarNumAleatorio(7, 4, B2);
-        B4 = generarNumAleatorio(7, 4, B2, B3);
-        B5 = generarNumAleatorio(7, 4, B2, B3, B4);
-        B6 = generarNumAleatorio(3, 1, B1);
-        B7 = generarNumAleatorio(3, 1, B1, B6);
+    private static boolean validarRespuesta(int respuesta, int correcta) {
+        if (respuesta == correcta) {
+            return true;
+        }
+        return false;
     }
     
-    public static String generarCadenaASCII(int largo) {
+    private static boolean validarRespuesta(String respuesta, String correcta) {
+        if (respuesta.equals(correcta)) {
+            return true;
+        }
+        System.out.println("Resposta incorrecta. Torna a intentar-ho.");
+        return false;
+    }
+    
+    private static void generarBolas() {
+        b1 = generarNumAleatorio(3, 1);
+        b2 = generarNumAleatorio(7, 4);
+        b3 = generarNumAleatorio(7, 4, b2);
+        b4 = generarNumAleatorio(7, 4, b2, b3);
+        b5 = generarNumAleatorio(7, 4, b2, b3, b4);
+        b6 = generarNumAleatorio(3, 1, b1);
+        b7 = generarNumAleatorio(3, 1, b1, b6);
+    }
+    
+    private static String generarCadenaAlfanumerica(int largo) {
         StringBuilder cadena = new StringBuilder();
         do {
             char random = (char)generarNumAleatorio(122, 48);
             if (random > 56 && random < 65 || random > 90 && random < 97) {
-                
                 continue;
             }
             cadena.append(random);
@@ -254,7 +189,7 @@ public class BolaDeDrac {
         return cadena.toString();
     }
     
-    public static String generarCadena(int largo) {
+    private static String generarCadenaVocales(int largo) {
         StringBuilder cadena = new StringBuilder();
         do {
             int random = generarNumAleatorio(VOCALES.length() - 1, 0);
@@ -263,12 +198,12 @@ public class BolaDeDrac {
         return cadena.toString();
     }
     
-    public static int generarNumAleatorio(int max, int min){
+    private static int generarNumAleatorio(int max, int min){
         int numero = (int) (Math.random()*(max-min +1)+ min);
         return numero;
     }
     
-    public static int generarNumAleatorio(int max, int min, int num) {
+    private static int generarNumAleatorio(int max, int min, int num) {
         int numero;
         do {
             numero = generarNumAleatorio(max, min);
@@ -276,7 +211,7 @@ public class BolaDeDrac {
         return numero;
     }
     
-    public static int generarNumAleatorio(int max, int min, int num1, int num2) {
+    private static int generarNumAleatorio(int max, int min, int num1, int num2) {
         int numero;
         do {
             numero = generarNumAleatorio(max, min);
@@ -284,7 +219,7 @@ public class BolaDeDrac {
         return numero;
     }
     
-    public static int generarNumAleatorio(int max, int min, int num1, int num2, int num3) {
+    private static int generarNumAleatorio(int max, int min, int num1, int num2, int num3) {
         int numero;
         do {
             numero = generarNumAleatorio(max, min);
@@ -292,10 +227,20 @@ public class BolaDeDrac {
         return numero;
     }
     
-    public static void imprimirMensaje(String mensaje) throws InterruptedException {
+    private static void imprimirMensaje(String mensaje) throws InterruptedException {
         for (int i = 0; i < mensaje.length(); i++) {
             System.out.print(mensaje.charAt(i));
             Thread.sleep(10);
         }
+        System.out.println();
+    }
+    
+    private static void mostrarFinal(boolean haGanado) {
+        if (haGanado) {
+            System.out.println("Enhorabona!! Heu aconseguit les 7 boles de Drac. El món torna a respirar tranquil. Fins un altra amics!");
+        } else {
+            System.out.println("Malauradament, la aventura ha acabat i el món torna a ser un lloc insegur. Una llàstima!");
+        }
+        System.out.println("Adéu.");
     }
 }
---
+++ src/main/java/ana/andrea/dragonball/Mensaje.java
@@ -0,0 +1,128 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ana.andrea.dragonball;
+
+/**
+ *
+ * @author Ana Carbonell Prieto
+ * @author Andrea Pérez Carbonell
+ */
+public class Mensaje {
+    public final static String INICIO = """
+                                        BENVINGUTS A L’AVENTURA DE DRAGON BALL
+                                        ========================================
+                                        
+                                        Son-Goku es troba tranquil·lament a casa junt al seu fill Son-Gohan, a
+                                        qui li està ajudant a resoldre uns deures que li han manat fer a l’escola,
+                                        concretament son de matemàtiques. De sobte, apareix a casa el Fullet
+                                        Tortuga i els conta que el malvat villà Freezer està planejant trobar les
+                                        7 boles del drac per tal de demanar un desig i obtindre un poder que
+                                        ningú podrà mai parar. El planeta està en perill. El Fullet Tortuga et
+                                        demana ajuda ¿Vols que Son-Goku i Son-Gohan accepten el repte?
+                                        """;
+    
+    public final static String LVL1 = """
+                                      Son Goku: Molt bé! Anem a buscar les boles de drac abans que
+                                      Freezer puga tenir-les. --va dir Son-Goku des de la seua casa en el
+                                      número %d de Ciutat del Nord.
+                                      
+                                      Goku va recordar que l’última vegada que les boles van ser repartides
+                                      pel món de manera aleatòria, una d’elles va anar a parar a la zona on
+                                      actualment viu el seu amic Satanàs Cor Petit i que probablement ell la
+                                      tinga en el seu poder. Van marxar doncs cap al número %d de la Ciutat
+                                      Meravella, que és on ell viu.
+                                      Després d’un llarg camí, hem arribat a la casa de Satanàs i, en efecte,
+                                      ell té una de les boles. Però no vol donar-nos-la gratuïtament, ja que li
+                                      té molta estima. Ens proposa la següent lluita matemática:
+                                      
+                                      Satanàs Cor Petit: Sabríeu dir-me quin és el sumatori entre el número
+                                      de la vostra casa i el número de la meua? Si l’endevineu, vos donaré
+                                      la bola i m’uniré al vostre equip.
+                                      """;
+    
+    public final static String LVL2 = """
+                                      Satanàs Cor Petit: Sou uns cracks de les matemàtiques! Pensava que
+                                      només sabíeu lluitar. Ací teniu la bola de %d estreles. Per cert, tinc el
+                                      número de telèfon de Ten Shin Han, qui em va enviar un WhatsApp la
+                                      setmana passada per contar-me que havia trobat la bola de %d estreles
+                                      en el Desert de l’Oblit. Anem-hi!
+                                      
+                                      Només arribar, trobaren a Ten Shin Han el qual no tenia el seu millor
+                                      dia i no els van rebre amb bona cara. Estava molt cabrejat perquè
+                                      estava intentant resoldre un enigma matemàtic i no veia forma de ferho.
+                                      
+                                      Son Gohan: Hola Ten Shin Han, necessitem que ens dones la teua
+                                      bola de drac.
+                                      
+                                      Ten Shin Han: Sí, un moment, resulta que porte així com 3 hores per
+                                      a descrifrar un enigma alfabètic anomenat “La mescla sagrada de
+                                      cadenes de text” i no veig la solució. Si m’ajudeu a resoldre’l, vos
+                                      donaré la bola. 
+                                      """;
+    
+    public final static String LVL3 = """
+                                      Ten Shin Han: Wow! Gràcies, ací teniu la bola de %d estreles. Per cert,
+                                      vaig saber que el nostre enemic Boo va trobar una bola de drac en la
+                                      seua mudança a “Muntanya Perduda”. Vos guiaré a la cova de la
+                                      muntanya on viu. No és lluny d’ací.
+                                      
+                                      Boo es trobava descansant a la cova. Ja que va tindre molts problemes
+                                      en els seus anteriors enfrontaments amb Goku aquest no tenia intenció
+                                      de lluitar més amb ell, però al veure’l què anava a per la seua estimada
+                                      bola de drac de %d estelles, no va reaccionar amb bones maneres.
+                                   
+                                      Boo: Què voleu? La meua bola de drac?
+                                   
+                                      Son Goku: Necessitem reunir-les totes abans que Freezer. El món
+                                      corre un gran perill! Sabem que tú ja no vols fer mal al planeta. T’ho
+                                      demanem per favor, pel bé de tots.
+                                      
+                                      Boo: Però és meua! La vaig trobar jo! Encara que pensant-ho millor,
+                                      només vos la donaré si em dieu la resposta correcta a la solució de
+                                      quin és el factorial de %d.
+                                      """;
+    
+    public final static String LVL4 = """
+                                      Boo: Vaja, vos ho he posat realment fácil. Ací teniu la bola. Ara bé, us
+                                      avance que encara teniu un os dur de rosegar. Cèl·lula també les
+                                      buscava i vaig escoltar en la tele que ja tenia unes quantes recollides.
+                                      Si no recorde mal, el vaig sentir a les notícies del canal número %d de
+                                      la meua televisió.
+                                      
+                                      Van sintonitzar el canal %d i van confirmar que Cèl·lula es trobava en
+                                      “Ciutat Nova”. Ràpidament van anar a enfrontar-se amb ell i així
+                                      aconseguir les boles que tenia en el seu poder.
+                                      
+                                      Son Gohan: Hola Cèl·lula, no volem problemes amb tu. Per favor,
+                                      dona'ns les teues boles de drac i no et farem mal.
+                                      
+                                      Cèl·lula: Jajaja. He entrenat des de l'última lluita i ara no podreu amb
+                                      mi i, per descomptat, no podreu llevar-me les boles del drac que tinc
+                                      guardades a aquesta caixa. Juguem a un joc, la caixa té una
+                                      contrasenya, però l’única manera d’aconseguir-la és desxifrant
+                                      aquesta endevinalla, juguem?
+                                      """;
+    
+    public final static String LVL5 = """
+                                      Cèl·lula: Maleïts sigueu! L’heu endevinat, però.... he de confessar-vos
+                                      que anava de fanal, i que no tinc cap bola de drac. El que sí puc dir-
+                                      vos es que la resta de boles que neccesiteu las té Freezer. Aneu a
+                                      molestar-lo a ell. Adèu!
+                                      
+                                      Son Goku: Bé, anem corrent al planeta de Freezer a llevar-li les boles
+                                      que resten. Esperem que surta tot bé.
+                                      
+                                      Van volar al Planeta Infernal on Freezer esperava al seus contrincants
+                                      per a la lluita final.
+                                      
+                                      Son Goku: Hola Freezer, sabem què trames. No ho permetrem.
+                                      Dona'ns les boles de drac %d, %d, %d i %d que sabem que tens.
+                                      
+                                      Freezer: De cap manera! Doneu-me vosaltres les vostres. Però mireu,
+                                      com no vull lluitar perquè ja tinc una edat, us done la possibilitat de
+                                      resoldre aquest conflicte amb una lluita matemàtica. Si sabeu dir-me
+                                      quin és el mínim comú múltiple de %d, %d, %d i %d tot haurà acabat.
+                                      """;
+}
